3f80a05a331207c04e944ee8140108200b318cc8
Mehdisaber332 <mehdisaber332@gmail.com>
2023-11-06 09:53:29 -0800

    start of file_storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import json
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
