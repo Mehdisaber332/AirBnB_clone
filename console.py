64035ca4d0ccdc4996c6adf89430ffafed3e3535
Mehdisaber332 <mehdisaber332@gmail.com>
2023-11-07 02:53:41 -0800

    Commands tested

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import cmd
+import models
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class definition"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        ''' Command to exit '''
+        return True
+
+    def do_EOF(self, arg):
+        ''' EOF Exit the program with CTRL+D '''
+        return True
+
+    def emptyline(self):
+        ''' Do nothing on an empty input line '''
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
